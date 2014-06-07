09388fda76fadba1b27a8c836ae8a116f9ba7ee1
Sangeetha Busangari <sangeetha.785@gmail.com>
2014-06-07 15:21:25 +0530

    Create SysbaseDB.java

---
+++ Pratise-session/Interface/SysbaseDB.java
@@ -0,0 +1,12 @@
+class SysbaseDB implements MyInter
+{
+	public void connect()
+	{
+		System.out.println("connected to SysbaseDB");
+	}
+	public void disconnect()
+	{
+		
+		System.out.println("disconnected to SysbaseDB");
+	}
+}
