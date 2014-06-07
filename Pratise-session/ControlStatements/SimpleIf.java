a0b55eed842d2e2883aad5619ad9e68b7a644e3f
Sangeetha Busangari <sangeetha.785@gmail.com>
2014-06-07 13:54:43 +0530

    Create SimpleIf.java

---
+++ Pratise-session/ControlStatements/SimpleIf.java
@@ -0,0 +1,17 @@
+class SimpleIf
+{
+	public static void main(String args[])
+	{
+		int a=10,b=5;
+		if(a<b)
+		System.out.println("a is less than b");
+		else
+		System.out.println("a is greater than b");
+	}
+}
+/*
+
+E:\JAVAprograms\ControlStatements>java SimpleIf
+a is greater than b
+
+E:\JAVAprograms\ControlStatements>*/
