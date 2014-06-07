f68f563293fcd79d7cc267575b435f65c5824d29
Sangeetha Busangari <sangeetha.785@gmail.com>
2014-06-07 14:32:23 +0530

    Create Multi.java

---
+++ Pratise-session/Inheritance/Multi.java
@@ -0,0 +1,21 @@
+class Multi
+{
+	public static void main(String args[])
+	{
+		Three t=new Three();
+		t.display();
+		t.show();
+	}
+}
+/*
+E:\JAVAprograms\Inheritance>java Multi
+sub class of i10
+accessing value using super keyword in subclass20
+sub class of i20
+accessing value using super keyword in subclass30
+super class of i30
+sub class of i20
+accessing value using super keyword in subclass30
+super class of i30
+
+E:\JAVAprograms\Inheritance>*/
