d8db50efeacb5f4b323241ce2fc194366b9e2164
Sangeetha Busangari <sangeetha.785@gmail.com>
2014-06-07 14:55:23 +0530

    Create Multi.java

---
+++ Pratise-session/Interface/Multi.java
@@ -0,0 +1,14 @@
+class Multi
+{
+	public static void main(String args[])
+	{
+		Child c=new Child();
+		c.height();
+	}
+}
+/*
+
+E:\JAVAprograms\Interfaces>java Multi
+child height6.0
+
+E:\JAVAprograms\Interfaces>*/
