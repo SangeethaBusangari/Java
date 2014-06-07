cd7cbb01b7f28071cf1bd28d16244d63ef93769e
Sangeetha Busangari <sangeetha.785@gmail.com>
2014-06-07 14:56:01 +0530

    Create Myclass.java

---
+++ Pratise-session/Interface/Myclass.java
@@ -0,0 +1,15 @@
+class Myclass implements B
+{
+	public void m1()
+	{
+		System.out.println("executing m1...");
+	}
+	public void m2()
+	{
+		System.out.println("executing m2...");
+	}
+	public void m3()
+	{
+		System.out.println("executing m3...");
+	}
+}
