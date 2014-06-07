5d47a7d984e94e41b51ef1524cabbecce0fe3886
Sangeetha Busangari <sangeetha.785@gmail.com>
2014-06-07 14:29:16 +0530

    Create Figure.java

---
+++ Pratise-session/Inheritance/Figure.java
@@ -0,0 +1,14 @@
+class Figure
+{
+	double x,y;
+	Figure(double x,double y)
+	{
+		this.x=x;
+		this.y=y;
+	}
+	double area()
+	{
+		System.out.println("area in figure class is undefined");
+		return 0;
+	}
+}
