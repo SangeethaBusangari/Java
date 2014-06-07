07419fec172d7785de7183debcb162300193ef19
Sangeetha Busangari <sangeetha.785@gmail.com>
2014-06-07 14:39:28 +0530

    Create Teacher.java

---
+++ Pratise-session/Inheritance/Teacher.java
@@ -0,0 +1,30 @@
+class Teacher
+{
+	int id;
+	String name;
+	float sal;
+	public void setId(int id)
+	{
+		this.id=id;
+	}
+	public int getId()
+	{
+		return id;
+	}
+	public void setName(String name)
+	{
+		this.name=name;
+	}
+	public String getName()
+	{
+		return name;
+	}
+	public void setSal(float sal)
+	{
+		this.sal=sal;
+	}
+	public float getSal()
+	{
+		return sal;
+	}
+}
