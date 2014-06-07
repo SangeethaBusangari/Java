0c593903d9a1dd8c2b495a98852da0b83b351726
Sangeetha Busangari <sangeetha.785@gmail.com>
2014-06-07 15:00:19 +0530

    Create NestedInterFace1.java

---
+++ Pratise-session/Interface/NestedInterFace1.java
@@ -0,0 +1,15 @@
+class NestedInterFace1
+{
+	public static void main(String args[])
+	{
+		InterfaceDemo1 obj=new InterfaceDemo1();
+		obj.add(20,30);
+		obj.sub(90,50);
+	}
+}
+/*
+E:\JAVAprograms\Interfaces>java NestedInterFace1
+addition=50
+subtraction=40
+
+E:\JAVAprograms\Interfaces>*/
