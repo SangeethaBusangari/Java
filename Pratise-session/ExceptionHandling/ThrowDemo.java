8e21332e838453a133adcd97090396ed4e68163b
Sangeetha Busangari <sangeetha.785@gmail.com>
2014-06-07 14:26:53 +0530

    Create ThrowDemo.java

---
+++ Pratise-session/ExceptionHandling/ThrowDemo.java
@@ -0,0 +1,14 @@
+
+public class ThrowDemo
+{
+	public static void main(String args[])
+	{
+		Sample2.demo();
+	}
+}
+/*
+E:\JAVAprograms\ExceptionHandling>java ThrowDemo
+Inside Demo()
+java.lang.NullPointerException: Exception data
+
+E:\JAVAprograms\ExceptionHandling>*/
