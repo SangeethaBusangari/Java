b5c3fc73517c8e9462f5e04ea21ac29980aa5bf6
Sangeetha Busangari <sangeetha.785@gmail.com>
2014-06-07 14:57:54 +0530

    Create NestedInter.java

---
+++ Pratise-session/Interface/NestedInter.java
@@ -0,0 +1,14 @@
+class NestedInter
+{
+	public static void main(String args[])
+	{
+		Sample.NestedInter nif=new IFace();
+		nif.max(13,4);
+	}
+}
+/*
+
+E:\JAVAprograms\Interfaces>java NestedInter
+ a is greater than b
+
+E:\JAVAprograms\Interfaces>*/
