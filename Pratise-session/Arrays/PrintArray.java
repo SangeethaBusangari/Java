00af0e0c8f07243bdfe18fddb7b230b6ca9dc581
Sangeetha Busangari <sangeetha.785@gmail.com>
2014-06-07 12:42:33 +0530

    Create PrintArray.java

---
+++ Pratise-session/Arrays/PrintArray.java
@@ -0,0 +1,22 @@
+import java.io.*;
+class PrintArray
+{
+	public static void main(String args[])
+	{
+		int a[]={1,2,3,4,5};
+		for(int i:a)
+		{
+			System.out.println(i);
+		}
+	}
+}
+/*
+
+E:\JAVAprograms\Arrays>java PrintArray
+1
+2
+3
+4
+5
+
+E:\JAVAprograms\Arrays>*/
