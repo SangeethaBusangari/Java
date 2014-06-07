53f9d3d10e9ac9bb7ef29c003572bb5ff88eb3fb
Sangeetha Busangari <sangeetha.785@gmail.com>
2014-06-07 14:53:30 +0530

    Create InterfaceDemo1.java

---
+++ Pratise-session/Interface/InterfaceDemo1.java
@@ -0,0 +1,11 @@
+class InterfaceDemo1 implements Samp.NestedInter1
+{
+	public void add(int x,int y)
+	{
+		System.out.println("addition="+(x+y));
+	}
+	public void sub(int a, int b)
+	{
+		System.out.println("subtraction="+(a-b));
+	}
+}
