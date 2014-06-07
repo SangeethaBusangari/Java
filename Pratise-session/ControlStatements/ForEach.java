568a2e22cd1520d7ab93a312ae1705adce608a6d
Sangeetha Busangari <sangeetha.785@gmail.com>
2014-06-07 13:52:42 +0530

    Create ForEach.java

---
+++ Pratise-session/ControlStatements/ForEach.java
@@ -0,0 +1,22 @@
+class ForEach
+{
+	public static void main(String args[])
+	{
+		int x[]={1,2,3,4,5};
+		for(int i:x)
+		System.out.println("x array is" +i);
+		
+	}
+}
+/*
+
+E:\JAVAprograms\ControlStatements>java ForEach
+x array is1
+x array is2
+x array is3
+x array is4
+x array is5
+
+E:\JAVAprograms\ControlStatements>
+
+*/
