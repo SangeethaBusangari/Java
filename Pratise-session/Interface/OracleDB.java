0ba8d7d3a17a2d794c5082e3a0ddfacd55f39a52
Sangeetha Busangari <sangeetha.785@gmail.com>
2014-06-07 15:02:29 +0530

    Create OracleDB.java

---
+++ Pratise-session/Interface/OracleDB.java
@@ -0,0 +1,12 @@
+class OracleDB implements MyInter
+{
+	public void connect()
+	{
+		System.out.println("connected to OracleDB");
+	}
+	public void disconnect()
+	{
+		
+		System.out.println("disconnected to OracleDB");
+	}
+}
