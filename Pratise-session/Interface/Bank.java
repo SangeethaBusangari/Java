69fb84ce7108d54196fb65f4a68925e2af3cc5ae
Sangeetha Busangari <sangeetha.785@gmail.com>
2014-06-07 14:47:15 +0530

    Create Bank.java

---
+++ Pratise-session/Interface/Bank.java
@@ -0,0 +1,15 @@
+class Bank implements CreditCard
+{
+	public double dollors(double d)
+	{
+		return (d*48.5);
+	}
+	public double pounds(double p)
+	{
+		return (p*75.3);
+	}
+	public double rial(double r)
+	{
+		return (r*14);
+	}
+}
