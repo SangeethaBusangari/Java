ee4f74f6bdb99def55ea7f155bf8b0e2db3f3e7d
Sangeetha Busangari <sangeetha.785@gmail.com>
2014-06-07 13:52:15 +0530

    Create DoWhile.java

---
+++ Pratise-session/ControlStatements/DoWhile.java
@@ -0,0 +1,23 @@
+class DoWhile
+{
+	public static void main(String args[])
+	{
+		int b=0;
+		do
+		{
+			System.out.println("b="+b);
+			b++;
+		}
+		while(b<=5);
+	}
+}
+/*
+
+E:\JAVAprograms\ControlStatements>java DoWhile
+b=0
+b=1
+b=2
+b=3
+b=4
+b=5
+*/
