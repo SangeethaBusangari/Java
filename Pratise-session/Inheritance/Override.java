3bccd6688f421415e338550e1546b99012a9371e
Sangeetha Busangari <sangeetha.785@gmail.com>
2014-06-07 14:33:50 +0530

    Create Override.java

---
+++ Pratise-session/Inheritance/Override.java
@@ -0,0 +1,20 @@
+class Override
+{
+	public static void main(String args[])
+	{
+		Two t=new Two();
+		t.show();
+	}
+}
+/*
+
+E:\JAVAprograms\Inheritance>javac One.java
+
+E:\JAVAprograms\Inheritance>javac Two.java
+
+E:\JAVAprograms\Inheritance>javac Override.java
+
+E:\JAVAprograms\Inheritance>java Override
+sub class of i20
+
+E:\JAVAprograms\Inheritance>*/
