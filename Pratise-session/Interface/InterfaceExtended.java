617ffb89a0acfdf8add223e0b9b16fb14e5673d3
Sangeetha Busangari <sangeetha.785@gmail.com>
2014-06-07 14:53:58 +0530

    Create InterfaceExtended.java

---
+++ Pratise-session/Interface/InterfaceExtended.java
@@ -0,0 +1,18 @@
+class InterfaceExtended
+{
+	public static void main(String args[])
+	{
+		Myclass m=new Myclass();
+		m.m1();
+		m.m2();
+		m.m3();
+	}
+}
+/*
+
+E:\JAVAprograms\Interfaces>java InterfaceExtended
+executing m1...
+executing m2...
+executing m3...
+
+E:\JAVAprograms\Interfaces>*/
