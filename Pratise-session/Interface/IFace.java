f1c3fa9e6165085c654e091f316c127189acf813
Sangeetha Busangari <sangeetha.785@gmail.com>
2014-06-07 14:51:49 +0530

    Create IFace.java

---
+++ Pratise-session/Interface/IFace.java
@@ -0,0 +1,10 @@
+class IFace implements Sample.NestedInter
+{
+	public void max(int a, int b)
+	{
+		if(a>b)
+			System.out.println(" a is greater than b");
+		else
+			System.out.println(" b is greater than a");
+	}
+}
