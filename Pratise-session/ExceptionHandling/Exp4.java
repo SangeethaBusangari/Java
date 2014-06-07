e173a33f0195c8eff9ac3f004dc70823ce18f3b2
Sangeetha Busangari <sangeetha.785@gmail.com>
2014-06-07 14:22:42 +0530

    Create Exp4.java

---
+++ Pratise-session/ExceptionHandling/Exp4.java
@@ -0,0 +1,22 @@
+import java.io.*;
+class Exp4
+{
+	public static void main(String args[])throws IOException
+	{
+		Sample1 s=new Sample1();
+		s.accept();
+		s.display();
+	}
+}
+/*
+E:\JAVAprograms\ExceptionHandling>javac Exp4.java
+
+E:\JAVAprograms\ExceptionHandling>java Exp4
+Enter your name
+sangeetha
+Namesangeetha
+
+E:\JAVAprograms\ExceptionHandling>
+
+
+*/
