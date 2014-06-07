4c5212a301898895184c3a9f6cf81990a0e57217
Sangeetha Busangari <sangeetha.785@gmail.com>
2014-06-07 13:58:28 +0530

    Create WhileLoop.java

---
+++ Pratise-session/ControlStatements/WhileLoop.java
@@ -0,0 +1,23 @@
+class WhileLoop
+{
+	public static void main(String args[])
+	{
+		int b=0;
+		while(b<=5)
+		{
+			System.out.println("b="+b);
+			b++;
+		}
+	}
+}
+/*
+
+E:\JAVAprograms\ControlStatements>java WhileLoop
+b=0
+b=1
+b=2
+b=3
+b=4
+b=5
+
+E:\JAVAprograms\ControlStatements>*/
