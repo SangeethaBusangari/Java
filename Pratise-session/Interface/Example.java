016c13cc45e56a3585cdd1a324dbd3c1ec2b7314
Sangeetha Busangari <sangeetha.785@gmail.com>
2014-06-07 14:49:54 +0530

    Create Example.java

---
+++ Pratise-session/Interface/Example.java
@@ -0,0 +1,17 @@
+class Example
+{
+	public static void main(String args[])
+	{
+		Imp i=new Imp();
+		i.max(18,4);
+		i.min(5,3);
+		i.square(4);
+	}
+}
+/*
+E:\JAVAprograms\Interfaces>java Example
+ a is greater than b
+ b is less than a
+square of the value16
+
+E:\JAVAprograms\Interfaces>*/
