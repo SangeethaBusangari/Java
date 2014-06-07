d46fe54a29ad0b71d02fde2de75ca935bcd36271
Sangeetha Busangari <sangeetha.785@gmail.com>
2014-06-07 13:49:30 +0530

    Create ADOrder.java

---
+++ Pratise-session/ControlStatements/ADOrder.java
@@ -0,0 +1,45 @@
+class ADOrder
+{
+	public static void main(String args[])
+	{
+		int n=Integer.parseInt(args[0]);
+		for(int i=0;i<=n;i++)
+		System.out.println("Ascending order is" +i);
+		for(int i=n;i>=0;i--)
+		System.out.println("descending order is"+i);
+		
+	}
+}
+/*
+
+E:\JAVAprograms\ControlStatements>java ADOrder 12
+enter cmd line argument
+Ascending order is0
+Ascending order is1
+Ascending order is2
+Ascending order is3
+Ascending order is4
+Ascending order is5
+Ascending order is6
+Ascending order is7
+Ascending order is8
+Ascending order is9
+Ascending order is10
+Ascending order is11
+Ascending order is12
+descending order is12
+descending order is11
+descending order is10
+descending order is9
+descending order is8
+descending order is7
+descending order is6
+descending order is5
+descending order is4
+descending order is3
+descending order is2
+descending order is1
+descending order is0
+
+E:\JAVAprograms\ControlStatements> */
+
