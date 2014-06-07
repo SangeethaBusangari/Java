86943623c6f48fe9d32f7c10ff6e4fe0b477bcd2
Sangeetha Busangari <sangeetha.785@gmail.com>
2014-06-07 12:33:57 +0530

    Create Different.java

---
+++ Pratise-session/Abstract-classes/Different.java
@@ -0,0 +1,20 @@
+class Different
+{
+	public static void main(String args[])
+	{
+		Sub1 obj1=new Sub1();
+		Sub2 obj2=new Sub2();
+		Sub3 obj3=new Sub3();
+		obj1.cal(10);
+		obj2.cal(5);
+		obj3.cal(4);
+	}
+}
+/*
+
+E:\JAVAprograms\AbstractClasses>java Different
+ sub1 10.0
+ sub2 25.0
+ sub3 2.0
+
+E:\JAVAprograms\AbstractClasses>*/
