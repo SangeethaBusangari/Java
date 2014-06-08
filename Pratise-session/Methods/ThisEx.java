3c0b84a07c1a87b91ed4d932c949d62236f60185
Sangeetha Busangari <sangeetha.785@gmail.com>
2014-06-08 22:06:19 +0530

    Create ThisEx.java

---
+++ Pratise-session/Methods/ThisEx.java
@@ -0,0 +1,14 @@
+class ThisEx
+{
+	public static void main(String args[])
+	{
+		This ob=new This();
+	}
+}
+/*
+E:\JAVAprograms\Methods>javac ThisEx.java
+
+E:\JAVAprograms\Methods>java ThisEx
+square value is25
+
+E:\JAVAprograms\Methods>*/
