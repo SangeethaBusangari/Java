28a44571fb5a1c5b23650c7d9fbc3e9b3453f2fa
Sangeetha Busangari <sangeetha.785@gmail.com>
2014-06-08 21:50:39 +0530

    Create NStaticEx.java

---
+++ Pratise-session/Methods/NStaticEx.java
@@ -0,0 +1,18 @@
+class NStaticEx
+{
+	public static void main(String args[])
+	{
+		NStatic ob=new NStatic();
+		ob.add();
+		ob.sub();//calling static method using object
+	}
+}
+/*
+E:\JAVAprograms\Methods>javac NStaticEx.java
+
+E:\JAVAprograms\Methods>java NStaticEx
+addition=15
+subtraction=5
+subtraction=5
+
+E:\JAVAprograms\Methods>*/
