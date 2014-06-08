026db2ec534563b6cbdca2a5ab9c5de765dc2a85
Sangeetha Busangari <sangeetha.785@gmail.com>
2014-06-08 21:44:46 +0530

    Create Factorial.java

---
+++ Pratise-session/Methods/Factorial.java
@@ -0,0 +1,14 @@
+class Factorial
+{
+	public static void main(String [] a)
+	{
+		int n=Integer.parseInt(a[0]);
+		System.out.println("factorial of a number is"+ Recursion.fact(n));
+	}
+}
+/*
+
+E:\JAVAprograms\Methods>java Factorial 4
+factorial of a number is24
+
+E:\JAVAprograms\Methods>*/
