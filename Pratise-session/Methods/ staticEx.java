6dd1bec67e6868cf3a2973ad5bb921a24501d53d
Sangeetha Busangari <sangeetha.785@gmail.com>
2014-06-08 22:02:19 +0530

    Create  staticEx.java

---
+++ Pratise-session/Methods/ staticEx.java
@@ -0,0 +1,19 @@
+class staticEx
+{
+	public static void main(String args[])
+	{
+		double x=Static.sum(2.5,6.5);
+		System.out.println("static result is"+ x);
+	}
+}
+/*
+
+E:\JAVAprograms\Methods>javac Static.java
+
+E:\JAVAprograms\Methods>javac StaticEx.java
+
+E:\JAVAprograms\Methods>java staticEx
+d=10;
+static result is9.0
+
+E:\JAVAprograms\Methods>*/
