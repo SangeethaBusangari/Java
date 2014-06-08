b61cb87192209111b9d579295dc5b8e94af00825
Sangeetha Busangari <sangeetha.785@gmail.com>
2014-06-08 21:46:03 +0530

    Create Method.java

---
+++ Pratise-session/Methods/Method.java
@@ -0,0 +1,21 @@
+class Method
+{
+	private int age;
+	private String name;
+	public void setName(String name)
+	{
+		this.name=name;
+	}
+	public void setAge(int age)
+	{
+		this.age=age;
+	}
+	public String getName()
+	{
+		return name;
+	}
+	public int getAge()
+	{
+		return age;
+	}
+}
