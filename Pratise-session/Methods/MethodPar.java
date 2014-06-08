ff8f726f4bc8f9f953f562cdba9b6e9a8d5282c0
Sangeetha Busangari <sangeetha.785@gmail.com>
2014-06-08 21:48:43 +0530

    Create MethodPar.java

---
+++ Pratise-session/Methods/MethodPar.java
@@ -0,0 +1,20 @@
+class MethodPar
+{
+	public static void main(String args[])
+	{
+		Method met=new Method();
+		met.setName("sangeetha");
+		met.setAge(21);
+		System.out.println("iam"+ met.getName());
+		System.out.println("my age is"+met.getAge());
+	}
+}
+/*
+
+E:\JAVAprograms\Methods>javac MethodPar.java
+
+E:\JAVAprograms\Methods>java MethodPar
+iamsangeetha
+my age is21
+
+E:\JAVAprograms\Methods>*/
