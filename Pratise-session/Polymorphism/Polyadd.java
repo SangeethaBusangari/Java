21a797836cdefd192da29ad1f354f9b459b4123d
Sangeetha Busangari <sangeetha.785@gmail.com>
2014-06-09 17:06:18 +0530

    Create Polyadd.java

---
+++ Pratise-session/Polymorphism/Polyadd.java
@@ -0,0 +1,11 @@
+class Polyadd
+{
+	void add(int a,int b)
+	{
+		System.out.println("addition of two numbers"+ (a+b));
+	}
+	void add(int a,int b, int c)
+	{
+		System.out.println("addition of three numbers"+(a+b+c));
+	}
+}
