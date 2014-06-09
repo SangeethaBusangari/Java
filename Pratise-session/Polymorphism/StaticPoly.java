0acf266e7aaa40ed17f000a4279c1f819057657b
Sangeetha Busangari <sangeetha.785@gmail.com>
2014-06-09 17:08:15 +0530

    Create StaticPoly.java

---
+++ Pratise-session/Polymorphism/StaticPoly.java
@@ -0,0 +1,14 @@
+class StaticPoly
+{
+	public static void main(String args[])
+	{
+		SuperStatic s=new SubStatic();
+		s.calculate(25);
+	}
+}
+/*
+
+E:\JAVAprograms\Polymorphism>java StaticPoly
+from base class625
+
+E:\JAVAprograms\Polymorphism>*/
