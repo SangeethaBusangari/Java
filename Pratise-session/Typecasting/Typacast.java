d355ff9e11d903ca0890d8b6d85d299ec13c0d55
Sangeetha Busangari <sangeetha.785@gmail.com>
2014-06-09 15:11:02 +0530

    Create Typacast.java

---
+++ Pratise-session/Typecasting/Typacast.java
@@ -0,0 +1,15 @@
+class Typacast
+{
+	public static void main(String args[])
+	{
+		One o=(One)new Two();
+		//One o=(Two)new Two();
+		o.show1();
+	}
+}
+/*
+
+E:\JAVAprograms\Typecasting>java Typacast
+super class method
+
+E:\JAVAprograms\Typecasting>*/
