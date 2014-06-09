ba3b51ae5929d261e603c164752dcc8c7bb61387
Sangeetha Busangari <sangeetha.785@gmail.com>
2014-06-09 15:14:13 +0530

    Create GetClassName.java

---
+++ Pratise-session/Typecasting/GetClassName.java
@@ -0,0 +1,12 @@
+class GetClassName
+{
+	public static void main(String args[])
+	{
+		MyClass obj=new MyClass(10);
+		KnowName.printName(obj);
+	}
+}
+/*
+E:\JAVAprograms\Typecasting>java GetClassName
+class name isMyClass
+*/
