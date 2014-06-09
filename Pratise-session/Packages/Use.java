0b35c2b78e2317c645f42e5b4a39b7c949fe1480
Sangeetha Busangari <sangeetha.785@gmail.com>
2014-06-09 17:00:06 +0530

    Create Use.java

---
+++ Pratise-session/Packages/Use.java
@@ -0,0 +1,18 @@
+import Orbit.Courses.Sample;
+class Use
+{
+	public static void main(String args[])
+	{
+		Sample s=new Sample();
+		s.show();
+	}
+}
+/*
+E:\JAVAprograms\packages>javac -d . Sample.java
+
+E:\JAVAprograms\packages>javac Use.java
+
+E:\JAVAprograms\packages>java Use
+Welcome to Orbit technology
+
+E:\JAVAprograms\packages>*/
