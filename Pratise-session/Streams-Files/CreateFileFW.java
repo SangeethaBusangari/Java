516d0349c8445f8e9c1c4d722e73f5be940cdf65
Sangeetha Busangari <sangeetha.785@gmail.com>
2014-06-09 15:43:47 +0530

    Create CreateFileFW.java

---
+++ Pratise-session/Streams-Files/CreateFileFW.java
@@ -0,0 +1,19 @@
+import java.io.*;
+class CreateFileFW
+{
+	public static void main(String args[])throws IOException
+	{
+		String str="Hyderabad is one of the beautiful city";
+	}
+}
+/*
+E:\Streams&Files>javac CreateFile.java
+
+E:\Streams&Files>java CreateFile
+enter text(@the end):
+Hi Bindhu
+Congratulations for your result
+cheers!!
+@
+
+E:\Streams&Files>*/	
