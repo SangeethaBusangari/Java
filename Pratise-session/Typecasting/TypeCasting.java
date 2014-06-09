639e000e2774a99f80f5fb0e024ad228ad6e811f
Sangeetha Busangari <sangeetha.785@gmail.com>
2014-06-09 15:10:33 +0530

    Create TypeCasting.java

---
+++ Pratise-session/Typecasting/TypeCasting.java
@@ -0,0 +1,16 @@
+class TypeCasting
+{
+	public static void main(String args[])
+	{
+		One o=new Two();
+		Two t=new Two();
+		o.show1();
+		t.show2();
+	}
+}
+/*
+E:\JAVAprograms\Typecasting>java TypeCasting
+super class method
+sub class method
+
+E:\JAVAprograms\Typecasting>*/
