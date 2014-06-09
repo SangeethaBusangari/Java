956b509b71778e0eb786232f63501b6fdf0c9078
Sangeetha Busangari <sangeetha.785@gmail.com>
2014-06-09 17:02:24 +0530

    Create DateImpl.java

---
+++ Pratise-session/Packages/DateImpl.java
@@ -0,0 +1,11 @@
+package Mypack;
+import Mypack.MyDate;
+import java.util.*;
+public class DateImpl implements MyDate
+{
+	public void showDate()
+	{
+		Date d=new Date();
+		System.out.println(d);
+	}
+}
