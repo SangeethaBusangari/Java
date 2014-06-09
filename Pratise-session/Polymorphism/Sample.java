4dab89fe2c256f1cebe6a730f9d7ee48329b74d0
Sangeetha Busangari <sangeetha.785@gmail.com>
2014-06-09 17:06:49 +0530

    Create Sample.java

---
+++ Pratise-session/Polymorphism/Sample.java
@@ -0,0 +1,17 @@
+class Sample
+{
+	public static void main(String args[])
+	{
+		Polyadd p=new Polyadd();
+		p.add(20,30);
+		p.add(10,20,30);
+	}
+}
+/*
+
+E:\JAVAprograms\Polymorphism>java Sample
+addition of two numbers50
+addition of three numbers60
+
+E:\JAVAprograms\Polymorphism>
+*/
