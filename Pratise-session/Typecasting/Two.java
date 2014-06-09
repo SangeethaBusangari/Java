28347f4721ebed7ea42356c2dfc46dbcca187191
Sangeetha Busangari <sangeetha.785@gmail.com>
2014-06-09 15:11:32 +0530

    Create Two.java

---
+++ Pratise-session/Typecasting/Two.java
@@ -0,0 +1,13 @@
+class Two extends One
+{
+	void show2()
+	{
+		System.out.println("sub class method");
+	}
+}
+/*
+if here fnname is show1
+E:\JAVAprograms\Typecasting>java Typacast
+sub class method
+
+E:\JAVAprograms\Typecasting>*/
